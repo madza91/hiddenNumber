4433fd4b3fd4781ca0a5089e04131de4028f328d
Nemanja Madžovski <madza91@live.com>
2019-02-20 23:30:14 +0100

    SSL

--- templates/default/includes/header.php
+++ templates/default/includes/header.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <title><?= $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="templates/default/assets/style.css" type="text/css" media="all">
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
